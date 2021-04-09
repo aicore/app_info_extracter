3d6df3e24a23f5433b6b3ccaa6b01dec5aee514e
dhruvpeter <dhruveldhopeter@gmail.com>
2021-04-09 15:24:53 +0530

    Added CLI

--- src/crawler/main.py
+++ src/crawler/main.py
@@ -1,4 +1,20 @@
 from scraper import Crawler
+import argparse
 
-scraper = Crawler('config.yaml')
+# scraper = Crawler('config.yaml')
+# scraper.crawl()
+
+parser =argparse.ArgumentParser(prog='App Info Extracter', usage='''python main.py <file> ''',
+description='''Description:
+App info extractor is a utility to extract and analyze customer reviews and critical metrics of apps from multiple
+sources like google play store, Apple play store, and anyother sources.This utility is highly configurable to extract
+and give relevant information about any app in a meaningful way.''',
+formatter_class=argparse.RawDescriptionHelpFormatter,
+add_help=True
+)
+parser.add_argument("file", type=str,
+help="config.yaml file containing the configuration for each app"
+)
+arg = parser.parse_args()
+scraper = Crawler(arg.file)
 scraper.crawl()
