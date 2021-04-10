59462efb257133ece467e133caa6859d077b12b8
dhruvpeter <dhruveldhopeter@gmail.com>
2021-04-10 13:29:06 +0530

    Changed {year} tag to 2021 in license header

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# (C) Copyright ${year} core.ai (https://core.ai/) 
+# (C) Copyright 2021 core.ai (https://core.ai/) 
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
